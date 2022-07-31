7b1c8d729c0df0317072dbce5caefa0c56d0ccbc
chhavitekriwal <chhavi.tekriwal@gmail.com>
2022-08-01 04:09:16 +0530

    remove user model

--- backend/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const user = new mongoose.Schema({
-    user_id: String,
-    userEmail: String,
-    posts: [{type:String}]
-});
-
-module.exports = mongoose.model('User',user);
