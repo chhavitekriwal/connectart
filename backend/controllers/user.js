358680dbee75de4abfeea3e76073de0721bb2ebc
chhavitekriwal <chhavi.tekriwal@gmail.com>
2022-08-01 07:20:24 +0530

    update user route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,23 @@
-const getUserDetails = (req, res) => {
+const Post = require('../models/post');
+
+const getUserDetails = async (req, res) => {
   try {
-    console.log('User ID: ', req.params.userId);
-    res.status(200).json({status: 'ok'});
+    const userID = req.params.userId;
+    const posts = await Post.find({postUserID: userID});
+    let maxUpvotes=0,totalUpvotes=0;
+    let bestPost;
+    posts.forEach(post => {
+      if(post.upvoteCount>maxUpvotes) {
+          bestPost = {
+          postLink: `https://i.imgur.com/${post.postID}`,
+          upvoteCount: post.upvoteCount
+        };
+        maxUpvotes = post.upvoteCount;
+      }
+      totalUpvotes+= post.upvoteCount;
+    })
+    if(!bestPost) throw new Error('No posts!');
+    res.status(200).json({bestPost,totalUpvotes});
   } catch (err) {
     res.status(500).json({message: err.message ?? "Server Error"});
   }
